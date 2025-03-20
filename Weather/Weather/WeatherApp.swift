039fa906d6a5b05ab6b64cc8ade752e5d8d0c91a
Artur Harutyunyan <arturhar@outlook.com>
2025-03-20 23:11:50 +0400

    Start

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Artur Harutyunyan on 20.03.25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
